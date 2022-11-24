8d2f7580b855f74483741b7bae62dae3ad404d91
Jeffrey Cheung <jeffreycheung_@live.com>
2022-11-23 19:23:28 -0800

    completed one stretch exercise to have function look for a word backwards and implemented npm test for it

--- wordsearch.js
+++ wordsearch.js
@@ -1,24 +1,35 @@
 const wordSearch = (letters, word) => {
-    if (letters.length === 0 || word.length === 0) {
-      return false;
-    }
-    const horizontalJoin = letters.map(ls => ls.join(''));
-    const verticalJoin = letters[0].map((col, index) =>
-      letters.map((row) => row[index])
-    );
-    const verticalSent = verticalJoin.map((ls) => ls.join(''));
-    for (let l of horizontalJoin) {
-      if (l.includes(word)) {
-        return true;
-      } else {
-        for (let l of verticalSent) {
-          if (l.includes(word)) {
-            return true;
+  if (letters.length === 0 || word.length === 0) {
+    return false;
+  }
+  
+  const horizontalJoin = letters.map(ls => ls.join(''));
+
+  const verticalJoin = letters[0].map((col, index) =>
+    letters.map((row) => row[index])
+  );
+  const verticalSent = verticalJoin.map((ls) => ls.join(''));
+
+  const reverse = letters.map(ls => ls.reverse().join(''));
+
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    } else {
+      for (let l of verticalSent) {
+        if (l.includes(word)) {
+          return true;
+        } else {
+          for (let l of reverse) {
+            if (l.includes(word)) {
+              return true;
+            }
           }
         }
       }
     }
-    return false;
-  };
-    
+  }
+  return false;
+};
+ 
 module.exports = wordSearch;
