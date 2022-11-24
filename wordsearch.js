9576398cd45a9931bb3fc6258b6e52eeb9023d59
Jeffrey Cheung <jeffreycheung_@live.com>
2022-11-23 19:05:07 -0800

    completed debugging code and added new npm test

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,24 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    if (letters.length === 0 || word.length === 0) {
+      return false;
     }
-}
-
-module.exports = wordSearch
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const verticalJoin = letters[0].map((col, index) =>
+      letters.map((row) => row[index])
+    );
+    const verticalSent = verticalJoin.map((ls) => ls.join(''));
+    for (let l of horizontalJoin) {
+      if (l.includes(word)) {
+        return true;
+      } else {
+        for (let l of verticalSent) {
+          if (l.includes(word)) {
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+  };
+    
+module.exports = wordSearch;
